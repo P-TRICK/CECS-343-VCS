4ef4da66f93550845e391f27ca261706c6c4de6f
Taylor Tobin <19tt94@gmail.com>
2016-03-16 17:42:12 -0700

    added Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+/*
+ * Taylor Tobin, Neil Wong, Patrick Izawa
+ * CECS 343 - VCS
+ */
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
